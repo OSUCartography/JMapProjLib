386eb6df47d20fef3bde6bde9a67bf680a9c3a35
Bojan <savricb@geo.oregonstate.edu>
2014-11-03 10:49:05 -0800

    Compact Miller inverse

--- src/com/jhlabs/map/proj/CompactMillerProjection.java
+++ src/com/jhlabs/map/proj/CompactMillerProjection.java
@@ -22,6 +22,11 @@ public class CompactMillerProjection extends CylindricalProjection {
     private final static double K1 = 0.9902;
     private final static double K2 = 0.1604;
     private final static double K3 = -0.03054;
+    private final static double C1 = K1;
+    private final static double C2 = 3 * K2;
+    private final static double C3 = 5 * K3;
+    private final static double EPS = 1e-11;
+    private final static double MAX_Y = 0.6 * Math.PI;
     
     @Override
     public Point2D.Double project(double lon, double lat, Point2D.Double out) {
@@ -33,7 +38,37 @@ public class CompactMillerProjection extends CylindricalProjection {
 
     @Override
     public boolean hasInverse() {
-        return false;
+        return true;
+    }
+
+    @Override
+    public Point2D.Double projectInverse(double x, double y, Point2D.Double lp) {
+
+        // make sure y is inside valid range
+        if (y > MAX_Y) {
+            y = MAX_Y;
+        } else if (y < -MAX_Y) {
+            y = -MAX_Y;
+        }
+
+        // latitude
+        double yc = y;
+        double tol;
+        for (;;) { // Newton-Raphson
+            double y2 = yc * yc;
+            double f = (yc * (K1 + y2 * (K2 + K3 * y2))) - y;
+            double fder = C1 + y2 * (C2 + C3 * y2);
+            yc -= tol = f / fder;
+            if (Math.abs(tol) < EPS) {
+                break;
+            }
+        }
+        lp.y = yc;
+
+        // longitude
+        lp.x = x;
+
+        return lp;
     }
 
     @Override
